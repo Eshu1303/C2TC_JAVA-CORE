166d3e1fad6364b9fbd46f8af30beed995a90a62
Eshu1303 <eshwa@Eshwari>
2024-04-01 15:22:18 +0530

    First commit

---
+++ C2TC_CoreJava/src/com/tnsif_day_01/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.tnsif_day_01;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ C2TC_CoreJava/src/com/tnsif_day_02/SecondProgram.java
@@ -0,0 +1,11 @@
+package com.tnsif_day_02;
+
+public class SecondProgram {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello New Day");
+
+	}
+
+}
+
